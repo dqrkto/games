7eb218922cb9ec21d76d3cca0610d4a846b8d389
dqrkto <84582260+dqrkto@users.noreply.github.com>
2021-11-13 10:50:32 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# favs
+# MyGames
--- js/in/scripts.js
+++ js/in/scripts.js
@@ -24,6 +24,31 @@ document.getElementById('boxAll').innerHTML = templateBox({
 	gamesJSON: gamesJSON,
 });
 
+function searchGame(textSearch) {
+	var contentUlSearch = '',
+		ulSearch = document.getElementById('ulSearch')
+
+	if (textSearch.length >= 3) {
+		for(gameOne of newGames){
+			if(gameOne.name.toUpperCase().indexOf(textSearch.toUpperCase()) > -1){
+				contentUlSearch += '<li><div class="row justify-content-center align-items-center w-100 m-0"><div class="col-3 col-lg-4 border-end border-secondary text-dark small">'
+				contentUlSearch += gameOne.platform
+				contentUlSearch += '</div><div class="col-9 col-lg-8 border-start border-secondary">'
+				contentUlSearch += gameOne.name
+				contentUlSearch += '</div></div></li>'
+			}
+		}
+		
+		ulSearch.innerHTML = contentUlSearch
+		ulSearch.classList.remove('d-none')
+	}
+
+	else{
+		ulSearch.classList.add('d-none')
+	}
+
+}
+
 /*
 
 2.5		|
